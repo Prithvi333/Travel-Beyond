d2f0369dd504c385a546e469c16beb4f62707ba8
Prithvi Chauhan <pthakur.pt36@gmail.com>
2023-06-14 19:46:45 +0530

    day_2 update by prithvi pull only needed code

--- entities/HotelType.java
+++
@@ -1,6 +0,0 @@
-package com.masaiproject.tripManagement.entities;
-
-public enum HotelType {
-
-    ONE_STAR,TWO_STAR,THREE_STAR,FOUR_STAR,FIVE_START
-}
--- entities/ReportType.java
+++
@@ -1,6 +0,0 @@
-package com.masaiproject.tripManagement.entities;
-
-public enum ReportType {
-
-    BUS_ISSUE,PACKAGE_ISSUE,TRIP_ISSUE,ROUTE_ISSUE,OTHER
-}
