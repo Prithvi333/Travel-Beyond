ca4e7799006a968bdbf87885c373a6e35a0d2ccb
Prithvi Chauhan <114485250+Prithvi333@users.noreply.github.com>
2023-06-15 20:43:51 +0530

    Delete entities directory
    Extra code removed

--- entities/HotelStatus.java
+++
@@ -1,6 +0,0 @@
-package com.masaiproject.tripManagement.entities;
-
-public enum HotelStatus {
-
-    OPEN,CLOSED,FILLED,VACANT
-}
--- entities/PaymentType.java
+++
@@ -1,6 +0,0 @@
-package com.masaiproject.tripManagement.entities;
-
-public enum PaymentType {
-
-    UPI,CREDIT_CARD,DEBIT_CARD,NET_BANKING
-}
